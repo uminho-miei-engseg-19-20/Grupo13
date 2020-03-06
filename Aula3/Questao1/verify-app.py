37aca06220e51a8e44ac714cf5ec7fd127dd99bc
KaiserFrost <brunossj5@hotmail.com>
2020-03-06 16:10:56 +0000

    Pergunta 1 aula3

---
+++ Aula3/Questao1/verify-app.py
@@ -0,0 +1,81 @@
+# coding: latin-1
+###############################################################################
+# eVotUM - Electronic Voting System
+#
+# verifySignature-app.py
+#
+# Cripto-7.4.1 - Commmad line app to exemplify the usage of verifySignature
+#       function (see eccblind.py)
+#
+# Copyright (c) 2016 Universidade do Minho
+# Developed by André Baptista - Devise Futures, Lda. (andre.baptista@devisefutures.com)
+# Reviewed by Ricardo Barroso - Devise Futures, Lda. (ricardo.barroso@devisefutures.com)
+#
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+###############################################################################
+"""
+Command line app that receives signer's public key from file and Data, Signature, Blind Components and
+prComponents from STDIN and writes a message to STDOUT indicating if the signature is valid..
+"""
+
+import sys
+from eVotUM.Cripto import eccblind
+from eVotUM.Cripto import utils
+
+def printUsage():
+    print("Usage: python verify-app.py -cert <certificado do assinante> -msg <mensagem original a assinar> -sDash <Signature> -f <ficheiro do requerente>")
+
+def parseArgs():
+    if(len(sys.argv) == 9 and sys.argv[1] == "-cert" and sys.argv[3] == "-msg" and sys.argv[5] == "-sDash" and sys.argv[7] == "-f"):
+        eccPublicKeyPath = sys.argv[2]
+        msgToSign = sys.argv[4]
+        signature = sys.argv[6]
+        reqFile = sys.argv[8]
+        main(eccPublicKeyPath,msgToSign,signature,reqFile)
+        
+    else:
+        printUsage()
+        
+
+def showResults(errorCode, validSignature):
+    print("Output")
+    if (errorCode is None):
+        if (validSignature):
+            print("Valid signature")
+        else:
+            print("Invalid signature")
+    elif (errorCode == 1):
+        print("Error: it was not possible to retrieve the public key")
+    elif (errorCode == 2):
+        print("Error: pR components are invalid")
+    elif (errorCode == 3):
+        print("Error: blind components are invalid")
+    elif (errorCode == 4):
+        print("Error: invalid signature format")
+
+def main(eccPublicKeyPath,msgToSign,signature,reqFile):
+    pemPublicKey = utils.readFile(eccPublicKeyPath)
+    filename = open(reqFile,'r')
+    lines = filename.readlines()
+    blindComponents = lines[0]
+    pRComponents = lines[1]
+    filename.close()
+    errorCode, validSignature = eccblind.verifySignature(pemPublicKey, signature, blindComponents, pRComponents, msgToSign)
+    showResults(errorCode, validSignature)
+
+if __name__ == "__main__":
+    parseArgs()
